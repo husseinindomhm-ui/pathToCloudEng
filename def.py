d3be21756f7f238b92162ad7252ba124af6b4457
husseinindomhm-ui <husseinindomhm@gmail.com>
2026-08-04 21:13:29 +0700

    main and return

--- def.py
+++ def.py
@@ -1,6 +1,6 @@
 # Def -- handmade function
 
-
+"""""
 def great(to = "world"):
     print(f"hello to: {to} !")
 
@@ -8,3 +8,26 @@ name = input("enter your name : ")
 if name == "":
     great()
 else: great(name)
+"""""
+def main():
+    name = input("enter your name : ")
+    if name == "":
+        great()
+    else: great(name)
+
+
+def great(to = "world"):
+    print(f"hello to: {to} !")
+
+main()
+
+"""""
+def main():
+    x = int(input("what is x ?"))
+    print("x squared is", square(x))
+
+def square(n):
+    return n * n
+
+main()
+"""""

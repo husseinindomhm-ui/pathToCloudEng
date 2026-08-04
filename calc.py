ed82aee602bc76e0c9147d378178e8653714bb09
husseinindomhm-ui <husseinindomhm@gmail.com>
2026-08-04 20:41:31 +0700

    float

--- calc.py
+++ calc.py
@@ -1,6 +1,35 @@
 # int
 
+"""""
 x = int(input("what is x ?"))
 y = int(input("what is y ?"))
 #z = int(x) + int(y)
 print(x+y)
+"""""
+
+# float
+"""""
+x = float(input("what is x ?"))
+y = float(input("what is y ?"))
+print(x+y)  
+"""""
+
+# round
+"""""
+x = float(input("what is x ?")) 
+y = float(input("what is y ?"))
+# print(round(x+y)
+z = round(x+y)
+print(f"{z:,}")
+"""""
+"""""
+x = float(input("what is x ?")) 
+y = float(input("what is y ?"))
+# print(round(x+y)
+# z = round(x / y, 2)
+z = x / y
+print(f"{z:.2f}")
+"""""
+
+
+

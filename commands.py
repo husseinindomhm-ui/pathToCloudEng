fb81a2d0c29e7f8e05b81a0832fdb77b283c6555
husseinindomhm-ui <husseinindomhm@gmail.com>
2026-08-04 21:51:30 +0700

    terminal commands

---
+++ commands.py
@@ -0,0 +1,15 @@
+# terminal commands 
+"""""
+code - create new file and open new tab of that file
+python or py - to run the python file 
+ls - to list all the fille in the directory
+cp - to make a copy of a file and give it a new name
+mv - to rename a file or move a file to another directory 
+rm - to remove a file
+mkdir - to make a directory "folder"
+cd - to change directory 
+rmdir - to remove a directory
+clear - is to clear the terminal window
+"""""
+
+
